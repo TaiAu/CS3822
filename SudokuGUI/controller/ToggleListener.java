61f4c79fc14bac37c05bd687bb33be9c1415c796
TaiAu <Shiu.Au.2014@live.rhul.ac.uk>
2016-12-02 00:01:44 +0000

    Create ToggleListener.java

---
+++ SudokuGUI/controller/ToggleListener.java
@@ -0,0 +1,40 @@
+package controller;
+
+import javax.swing.JToggleButton;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
+/**
+ * The ToggleListener class is responsible for registering state changes and
+ * configuring the buttons to the listeners.
+ * 
+ * @author Tai
+ */
+public class ToggleListener implements ChangeListener {
+
+	private JToggleButton[] toggleButtons;
+
+	/**
+	 * The configureButtons method is responsible for adding listeners to the
+	 * buttons in the ButtonPanel class.
+	 * 
+	 * @param buttons
+	 */
+	public void configureButtons(JToggleButton... buttons) {
+		toggleButtons = new JToggleButton[buttons.length];
+		for (int i = 0; i < buttons.length; i++) {
+			toggleButtons[i] = buttons[i];
+		}
+	}
+
+	public void stateChanged(ChangeEvent event) {
+		JToggleButton toggleButton = (JToggleButton) event.getSource();
+		if (toggleButton.isSelected()) {
+			for (JToggleButton button : toggleButtons) {
+				if (!button.equals(toggleButton)) {
+					button.setSelected(false);
+				}
+			}
+		}
+	}
+}
