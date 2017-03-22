467599af39ab2b2e308910fff520eab2e4e0791c
TaiAu <Shiu.Au.2014@live.rhul.ac.uk>
2017-03-22 04:55:55 +0000

    JavaDoc updates

--- SudokuGUI/view/SudokuFrame.java
+++ SudokuGUI/view/SudokuFrame.java
@@ -13,16 +13,16 @@ import javax.swing.JPanel;
 import model.SudokuPuzzle;
 
 /**
- * The Sudoku Frame in charge of initialising the panel.
+ * The Sudoku Frame responsible for initialising the Sudoku panel.
  * 
  * @author Tai
  */
 public class SudokuFrame {
 
 	private ButtonPanel buttonPanel;
-	private JFrame frame;
+	private JFrame sudokuFrame;
 	private SudokuPanel sudokuPanel;
-	private SudokuPuzzle sudokuPuzzle;
+	private SudokuPuzzle model;
 
 	/**
 	 * The SudokuFrame constructor.
@@ -30,24 +30,79 @@ public class SudokuFrame {
 	 * @param sudokuPuzzle
 	 *            the puzzle
 	 */
-	public SudokuFrame(SudokuPuzzle sudokuPuzzle) {
-		this.sudokuPuzzle = sudokuPuzzle;
-		initialisePanel();
+	public SudokuFrame(SudokuPuzzle model) {
+		this.model = model;
+		createPartControl();
+	}
+
+	/**
+	 * Creates and initialises the Sudoku and Button Panels with JFrames for
+	 * each.
+	 */
+	private void createPartControl() {
+		sudokuPanel = new SudokuPanel(this, model);
+		buttonPanel = new ButtonPanel(this, model);
+
+		sudokuFrame = new JFrame();
+		sudokuFrame.setTitle("Sudoku Puzzle Solver");
+		sudokuFrame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+		sudokuFrame.addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowClosing(WindowEvent event) {
+				exitProcedure();
+			}
+		});
+
+		JPanel mainPanel = new JPanel();
+		mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.X_AXIS));
+		mainPanel.add(sudokuPanel);
+
+		JPanel holderPanel = new JPanel();
+		holderPanel.setLayout(new FlowLayout());
+		holderPanel.add(buttonPanel.getPanel());
+		mainPanel.add(holderPanel);
+
+		sudokuFrame.setLayout(new FlowLayout());
+		sudokuFrame.add(mainPanel);
+		sudokuFrame.pack();
+		sudokuFrame.setBounds(getBounds());
+		sudokuFrame.setVisible(true);
+	}
+
+	/**
+	 * Closes the frame.
+	 */
+	public void exitProcedure() {
+		sudokuFrame.dispose();
+		System.exit(0);
+	}
+
+	/**
+	 * Gets the bounds of the graphical object.
+	 * 
+	 * @return the x and y bounds
+	 */
+	protected Rectangle getBounds() {
+		Rectangle f = sudokuFrame.getBounds();
+		Rectangle w = GraphicsEnvironment.getLocalGraphicsEnvironment().getMaximumWindowBounds();
+		f.x = (w.width - f.width) / 2;
+		f.y = (w.height - f.height) / 2;
+		return f;
 	}
 
 	/**
 	 * Gets the Frame.
 	 * 
-	 * @return
+	 * @return the sudoku frame
 	 */
 	public JFrame getFrame() {
-		return frame;
+		return sudokuFrame;
 	}
 
 	/**
 	 * Gets the Sudoku Panel.
 	 * 
-	 * @return
+	 * @return the sudoku panel
 	 */
 	public JPanel getSudokuPanel() {
 		return sudokuPanel;
@@ -58,60 +113,5 @@ public class SudokuFrame {
 	 */
 	public void repaintSudokuPanel() {
 		sudokuPanel.repaint();
-	}
-
-	/**
-	 * The initialisePanel method creates and initialises the Sudoku and Button
-	 * Panels with JFrames for each.
-	 */
-	private void initialisePanel() {
-		buttonPanel = new ButtonPanel(this, sudokuPuzzle);
-		sudokuPanel = new SudokuPanel(this, sudokuPuzzle);
-
-		frame = new JFrame();
-		frame.setTitle("Sudoku Solver");
-		frame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
-		frame.addWindowListener(new WindowAdapter() {
-			@Override
-			public void windowClosing(WindowEvent event) {
-				exitProcedure();
-			}
-		});
-
-		JPanel mainPane = new JPanel();
-		mainPane.setLayout(new BoxLayout(mainPane, BoxLayout.X_AXIS));
-		mainPane.add(sudokuPanel);
-
-		JPanel outerPane = new JPanel();
-		outerPane.setLayout(new FlowLayout());
-		outerPane.add(buttonPanel.getPanel());
-		mainPane.add(outerPane);
-
-		frame.setLayout(new FlowLayout());
-		frame.add(mainPane);
-		frame.pack();
-		frame.setBounds(getBounds());
-		frame.setVisible(true);
-	}
-
-	/**
-	 * Closes the frame.
-	 */
-	public void exitProcedure() {
-		frame.dispose();
-		System.exit(0);
-	}
-
-	/**
-	 * Gets the bounds of the graphics object.
-	 * 
-	 * @return the x and y bounds
-	 */
-	protected Rectangle getBounds() {
-		Rectangle rectangleFrame = frame.getBounds();
-		Rectangle w = GraphicsEnvironment.getLocalGraphicsEnvironment().getMaximumWindowBounds();
-		rectangleFrame.x = (w.width - rectangleFrame.width) / 2;
-		rectangleFrame.y = (w.height - rectangleFrame.height) / 2;
-		return rectangleFrame;
 	}
 }
